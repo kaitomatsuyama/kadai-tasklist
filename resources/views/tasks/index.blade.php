33b7a4076d875e9bd26f1160ec8276fa5addf91e
kaito matsuyama <songshanhaixiang8@gmail.com>
2020-11-30 06:47:01 +0000

    Link update

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -21,6 +21,14 @@
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
+                
+                @foreach ($tasks as $task)
+                <tr>
+                    {{-- タスク詳細ページへのリンク --}}
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
             </tbody>
         </table>
     @endif
